75cf879650f960e197feda3a1901119b130782bf
Pooja-Jathar1406 <113439441+Pooja-Jathar1406@users.noreply.github.com>
2022-10-29 23:56:41 +0530

    Delete OOPJ directory

--- OOPJ/Assignments/CmdLineArg.java
+++
@@ -1,7 +0,0 @@
-class CmdLineArg{
-	
-	public static void main(String args[]){
-		System.out.println("Welcome "+args[0]);           // while running we need to pass args :   java CmdLineArg Pooja
-	}
-}
-o/p: Welcome Pooja
