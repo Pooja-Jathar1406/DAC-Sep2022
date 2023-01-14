9a04b9014ecc2b43977acda241e9321f1583491a
Pooja-Jathar1406 <113439441+Pooja-Jathar1406@users.noreply.github.com>
2023-01-14 20:42:02 +0530

    Add files via upload

---
+++ dotNet/Assignments/Assign4/Question1.cs
@@ -0,0 +1,52 @@
+﻿namespace Assign4
+{
+    internal class Question1
+    {
+        static void Main1(string[] args)
+        {
+            /* ------------------------ Question 1 -----------------------*/
+
+            Console.WriteLine("Welcome to Cdac Mumbai !!!");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Enter no of batches ");
+            int batch = int.Parse(Console.ReadLine());
+
+            int[][] arr = new int[batch][];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+
+                Console.WriteLine($"Enter no students for Batch {i + 1}:");
+                int s = int.Parse(Console.ReadLine());
+
+                Student[] stud = new Student[s];
+                for (int j = 0; j < stud.Length; j++)
+                {
+                    Console.WriteLine("Enter marks for student  {0}: ", j + 1);
+                    int m = int.Parse(Console.ReadLine());
+
+                    stud[j] = new Student();
+                    stud[j].marks = m;
+                }
+
+                Console.WriteLine("Student marks of Batch {0}", i + 1);
+
+                Console.WriteLine("Marks of Students : ");
+
+                foreach (Student std in stud)
+                {
+                    Console.Write(std.marks + " ");
+                }
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+    public class Student
+    {
+        public int marks;
+
+    }
+
+}
---
+++ dotNet/Assignments/Assign4/Question2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4
+{
+    internal class Question2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Details!!!");
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Enter no of employees ");
+            int noOfEmp = int.Parse(Console.ReadLine());
+
+            Employee[] empArr = new Employee[noOfEmp];
+
+
+            /*--------- Enter employee details ---------*/
+            for (int i = 0; i < empArr.Length; i++)
+            {
+                Console.WriteLine("Enter details for Employee " + (i + 1) + ":");
+
+                Console.WriteLine("Enter EmpNo: ");
+                int empNo = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Name: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter Salary: ");
+                int salary = int.Parse(Console.ReadLine());
+
+                empArr[i] = new Employee(empNo, name, salary);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Details of all employees!!!1");
+            Console.WriteLine("-----------------");
+            foreach (Employee emp in empArr)
+            {
+                Console.Write(emp.EmpNo + " ");
+                Console.Write(emp.Name + " ");
+                Console.Write(emp.Salary + " ");
+                Console.WriteLine("----------------- ");
+            }
+
+            /*--------- Employee with highest salary ---------*/
+            int highestSal = empArr[0].Salary;
+            int highestSalIndex = 0;
+
+            for (int i = 1; i < empArr.Length; i++)
+            {
+                if (empArr[i].Salary > highestSal)
+                    highestSal = empArr[i].Salary;
+                highestSalIndex = i;
+            }
+            Console.WriteLine();
+            Console.Write("Highest salary : "+highestSal);
+
+            Console.WriteLine();
+
+            Console.WriteLine("------- Details of Employee with Highest Salary ---------");
+            Console.WriteLine($"Employee {highestSalIndex + 1}: Id: " + empArr[highestSalIndex].EmpNo + " Name : " + empArr[highestSalIndex].Name + " Salary :" + empArr[highestSalIndex].Salary);
+
+
+            /*--------- Search employee ---------*/
+
+            Console.WriteLine();
+            Console.WriteLine("Enter EmpNo to search the employee: ");
+            int empNoTosearch = int.Parse(Console.ReadLine());
+            for (int i = 1; i < empArr.Length; i++)
+            {
+                if (empArr[i].EmpNo == empNoTosearch)
+                {
+                    Console.WriteLine($"Employee {i + 1}: Id: " + empArr[i].EmpNo + " Name : " + empArr[i].Name + " Salary :" + empArr[i].Salary);
+                }
+
+            }
+        }
+
+        public class Employee
+        {
+            public int EmpNo { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
+            public Employee(int empNo, string name, int salary)
+            {
+                EmpNo = empNo;
+                Name = name;
+                Salary = salary;
+            }
+        }
+    }
+}
