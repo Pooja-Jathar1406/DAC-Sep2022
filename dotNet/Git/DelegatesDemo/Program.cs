298e3483ebafb6c1afe074827791f1812cd40eb5
Pooja-Jathar1406 <113439441+Pooja-Jathar1406@users.noreply.github.com>
2023-01-22 23:39:08 +0530

    Add files via upload

---
+++ dotNet/Git/DelegatesDemo/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace DelegatesDemo
+{
+    //step1; create a delegate class having the same sign as the fun to be called
+    public delegate void Del1();
+    public delegate int Del2(int a, int b);
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            //step 2: create a object of delegate class and pass function to all (function name ) as a parameter
+            Del1 objDel = new Del1(Display);
+
+            //step3: call the function via the delegate reference
+            objDel();
+        }
+
+        //another simpler way for delegates and =+ , -=
+        static void Main2(string[] args)
+        {
+            Del1 objDel = Display;
+            objDel();
+
+            Console.WriteLine();
+            objDel += Show;
+            objDel();
+
+            Console.WriteLine();
+            objDel += Display;
+            objDel();
+
+            //Console.WriteLine();
+            //objDel -= Show;
+            //objDel();
+
+            Console.WriteLine();
+            objDel -= Display; 
+            objDel();
+
+        }
+
+        //using inbult methods
+        static void Main3() {
+
+            Del1 objDel=(Del1)Delegate.Combine(new Del1(Display), new Del1(Show), new Del1(Display));
+            objDel();
+
+            Console.WriteLine();
+            objDel = (Del1)Delegate.Remove(objDel, new Del1(Display));
+            objDel();
+
+            Console.WriteLine();
+            objDel = (Del1)Delegate.RemoveAll(objDel, new Del1(Display));
+            objDel();
+
+
+        }
+
+
+        //delegates for parameterised functions
+        static void Main4() {
+
+            Del2 objDel2 = Add;
+            int ans = objDel2(1,2);
+            Console.WriteLine(ans);
+
+            Console.WriteLine(objDel2(10,30));
+        }
+
+        //delegates for static function of another class
+        static void Main() {
+
+            Del1 objDel3 = Class1.Display1;
+            objDel3();
+
+            Class1 obj = new Class1();
+            objDel3 = obj.Show1;
+            objDel3();
+
+            Class1 obj2 = new Class1();
+            Del2 objDel4 = obj2.Add1; 
+            Console.WriteLine(objDel4(10, 30));
+            
+            objDel4 = obj2.Subtract1;
+            Console.WriteLine(objDel4(40, 30));
+        }
+
+        static void Display() {
+            Console.WriteLine("Display function inside Program class is called.");
+        }
+
+        static void Show()
+        {
+            Console.WriteLine("Show function inside Program class is called.");
+        }
+
+        static int  Add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+    public class Class1
+    { 
+        public static void Display1()
+        {
+            Console.WriteLine("Display1 function of class1");
+        }
+        
+        public void Show1()
+        {
+            Console.WriteLine("Show1 function of class1");
+        }
+
+        public int Add1(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract1(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ dotNet/Git/ExceptionHandling2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExceptionHandling2
+{
+    internal class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            Class1 objClass1 = new Class1();
+            objClass1.InvalidP1 += objClass1_InvalidP1;
+            //+= idicates if there are already defined other event handlers are present then add this event otherwise =
+
+            objClass1.P1= 1000;
+        }
+        static void objClass1_InvalidP1() {
+            Console.WriteLine("Event Handling code");
+        }
+        */
+
+        // the above code can be written in more easier way as
+        static void Main() { 
+            Class1 obj = new Class1();
+            obj.InvalidP1 += Obj_InvalidP1;
+        }
+
+        private static void Obj_InvalidP1()
+        {
+            Console.WriteLine("Event handled here");
+        }
+    }
+    //step 1: create a delegate class having the same signature as the event handler
+    public delegate void InvalidP1EventHandler();
+
+    public class Class1 {
+
+        //step2: create a delegate object (event)
+        public event InvalidP1EventHandler InvalidP1; 
+        //here the word event is required just to show that this is not normal delegate obj , it is for event
+
+        private int p1;
+        public int P1 
+        {
+            get{
+                return p1;
+            }
+            set {
+                if (value < 100)
+                {
+                    p1 = value;
+                }
+                else {
+                    //step 3 : raise the event - call the delegat object
+                    InvalidP1();
+                }
+            }
+        }
+    }
+    //public class InvalidP1Exception : ApplicationException {
+    //    public InvalidP1Exception(string message) : base(message){ 
+
+    //    }  
+    //}
+}
