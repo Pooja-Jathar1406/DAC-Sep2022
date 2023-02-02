686b7e382fb570f7b141a9125a053e1819b67646
Pooja-Jathar1406 <113439441+Pooja-Jathar1406@users.noreply.github.com>
2023-02-02 12:29:26 +0530

    Add files via upload

---
+++ SE/Testing/lecture content/DAC/src/SESSION1/Script1.java
@@ -0,0 +1,25 @@
+package SESSION1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Script1 {
+	
+	ChromeDriver driver;
+
+	//to open a browser
+	public void open()
+	{
+		System.setProperty("webdriver.chrome.driver", "E:\\EclipseWorkspace\\lib\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/");  //open
+		driver.manage().window().maximize(); //maximise
+		driver.close();//close
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Script1 o=new Script1();
+o.open();
+	}
+
+}
